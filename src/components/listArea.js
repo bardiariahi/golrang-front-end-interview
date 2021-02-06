910a715607fb0cd5080ec8552a2d3c581e805fcd
Bardia Riahi <bardiariahi@gmail.com>
2021-02-06 21:47:33 +0330

    React App

---
+++ src/components/listArea.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ListArea extends Component {
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        );
+    }
+}
+
+export default ListArea;
---
+++ src/view/main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ListArea from "../components/listArea";
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <ListArea />
+            </div>
+        );
+    }
+}
+
+export default Main;
